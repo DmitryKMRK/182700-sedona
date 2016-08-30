d31c8be2fe6ab1a0d853f0d9950cabf13f190abc
DmitryS1991 <dmitry.kmrk@gmail.com>
2016-08-30 15:40:19 +0300

    Доработка проекта

---
+++ js/popup.js
@@ -0,0 +1,7 @@
+var button = document.querySelector(".booking-button");
+var popup = document.querySelector(".form-window");
+
+button.addEventListener("click", function(event) {
+event.preventDefault();
+popup.classList.toggle("form-window-show");
+});
